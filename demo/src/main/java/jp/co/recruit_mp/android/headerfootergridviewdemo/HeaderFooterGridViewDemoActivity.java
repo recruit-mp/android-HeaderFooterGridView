8166065794db8bcd76b17447c82cc87738a11707
Yuichi Hirano <yhirano@r.recruit.co.jp>
2014-10-09 16:23:13 +0900

    Update file header comment.

--- demo/src/main/java/jp/co/recruit_mp/android/headerfootergridviewdemo/HeaderFooterGridViewDemoActivity.java
+++ demo/src/main/java/jp/co/recruit_mp/android/headerfootergridviewdemo/HeaderFooterGridViewDemoActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014  Recruit Marketing Partners Co.,Ltd
+ * Copyright (C) 2014 Recruit Marketing Partners Co.,Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
